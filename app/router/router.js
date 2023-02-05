e6131873570d0366269c09bf8558f2c01a62d8f9
RoseErrik-FS <errose@student.fullsail.edu>
2023-02-04 22:08:04 -0600

    Added more functionality in the form of Patch, Post, Delete

--- app/router/router.js
+++ app/router/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// calling http://localhost:3000/exercise
+// Calling http://localhost:3000/exercise with a GET Request
 router.get("/", (req, res, next) => {
   res.status(200).json({
     message: "Success - GET",
@@ -10,20 +10,56 @@ router.get("/", (req, res, next) => {
       method: req.method,
     },
   });
-}),
+});
 
-//calling http://localhost:3000/exercise/34
+// Calling http://localhost:3000/exercise/34 with a Get by ID request
 router.get("/:id", (req, res, next) => {
-    const id = req.params.id;
+  const id = req.params.id;
+  res.status(200).json({
+    message: "Success - GET by ID",
+    id: id,
+    metadata: {
+      hostname: req.hostname,
+      method: req.method,
+    },
+  });
+});
 
-    res.status(200).json({
-        message: "Success - GET by ID",
-        id: id,
-        metadata: {
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
-  }),
+// Calling http://localhost:3000/exercise/12 with a patch request
+router.patch("/:id", (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: "Success - PATCH",
+    id: id,
+    metadata: {
+      hostname: req.hostname,
+      method: req.method,
+    },
+  });
+});
 
-  (module.exports = router);
+// Calling http://localhost:3000/exercise with a post request
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Success - POST",
+    metadata: {
+      hostname: req.hostname,
+      method: req.method,
+    },
+  });
+});
+
+// Calling http://localhost:3000/exercise/12 with a delete request
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: "Success - DELETE",
+    id: id,
+    metadata: {
+      hostname: req.hostname,
+      method: req.method,
+    },
+  });
+});
+
+module.exports = router;
